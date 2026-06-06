80e79c8fe2060dcd0675313c6ea71c78b3386cf0
muhiqsimui <muhiqsimui@gmail.com>
2026-06-06 10:14:04 +0800

    setup awal proyek

---
+++ Makefile
@@ -0,0 +1,8 @@
+dev:
+	cd astro && npm run dev
+
+generator:
+	cd generator && go run ./cmd
+
+build:
+	cd astro && npm run build
---
+++ astro/astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://muhiqsimui.github.io",
+  base: "/nexasnap",
+  trailingSlash: "never",
+
+  integrations: [sitemap()],
+});
